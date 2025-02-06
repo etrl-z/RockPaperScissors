e59dc42580957b8c79886774acda676a6821810a
etrl-z <emiliotorella@gmail.com>
2025-02-06 12:38:06 +0100

    added image transtition

--- RPS.js
+++ RPS.js
@@ -1,6 +1,9 @@
 const moves = ["r", "p", "s"];
+const toWin = 5;
 var pointsP1 = 0;
 var pointsP2 = 0;
+var scoring = "";
+var color = "";
 var enableMove = true;
 let confettiInterval;
 
@@ -10,33 +13,38 @@ function move(myMove) {
 
     var rndMove = generateMove();
     var result = compareMoves(myMove, rndMove);
-    var winner = "";
-    var color = "orange";
+
     if (result == 1) {
         pointsP1++;
-        winner = "HUMAN";
+        scoring = "HUMAN";
         color = "green";
     }
     else if (result == 2) {
         pointsP2++;
-        winner = "COMPUTER";
+        scoring = "COMPUTER";
         color = "red";
     }
+    else{
+        color = "orange";
+    }
 
-    document.getElementById("screen-1").innerHTML = stringifyMove(myMove);
-    document.getElementById("screen-2").innerHTML = stringifyMove(rndMove);
+    changeImg(document.getElementById("screen-1-move"), myMove);
+    changeImg(document.getElementById("screen-2-move"), rndMove);
+
     document.getElementById("points").innerHTML = pointsP1 + " - " + pointsP2;
     document.getElementById("btn").style = "display: block";
 
-    if (pointsP1 == 10 || pointsP2 == 10) {
-        document.getElementById("message").innerHTML = winner + " WINS!";
+    if (pointsP1 == toWin || pointsP2 == toWin) {
+        document.getElementById("message").innerHTML = scoring + " WINS!";
         document.getElementById("message").style.color = color;
 
         enableMove = false;
-        confettiInterval = setInterval(createConfetti, 5);
+        if (pointsP1 == toWin) {
+            confettiInterval = setInterval(createConfetti, 5);
+        }
     }
     else {
-        document.getElementById("message").innerHTML = result == 0 ? "DRAW!" : winner + " SCORES!";
+        document.getElementById("message").innerHTML = result == 0 ? "DRAW!" : scoring + " SCORES!";
         document.getElementById("message").style.color = color;
     }
 }
@@ -46,20 +54,28 @@ function generateMove() {
     return moves[rnd];
 }
 
-function stringifyMove(move) {
-    var str = "";
+function changeImg(image, move) {
+    image.style.transform = "scale(0.01)";
+    setTimeout(() => {
+        image.src = getImgFromMove(move);
+        image.style.transform = "scale(0.5)";
+    }, 100);
+}
+
+function getImgFromMove(move) {
+    var src = "";
     switch (move) {
         case "r":
-            str = "ROCK";
+            src = "images/rock.jpg";
             break;
         case "p":
-            str = "PAPER";
+            src = "images/paper.jpg";
             break;
         case "s":
-            str = "SCISSORS";
+            src = "images/scissors.jpg";
             break;
     }
-    return str;
+    return src;
 }
 
 function compareMoves(myMove, pcMove) {
@@ -78,13 +94,13 @@ function reset() {
     pointsP1 = 0;
     pointsP2 = 0;
     enableMove = true;
-    document.getElementById("screen-1").innerHTML = "";
-    document.getElementById("screen-2").innerHTML = "";
+    document.getElementById("screen-1-move").src = "";
+    document.getElementById("screen-2-move").src = "";
     document.getElementById("message").innerHTML = "MAKE YOUR MOVE";
     document.getElementById("message").style.color = "black";
     document.getElementById("points").innerHTML = pointsP1 + " - " + pointsP2;
     document.getElementById("btn").style = "display: none";
-    
+
     stopConfetti();
 }
 
@@ -92,25 +108,25 @@ function createConfetti() {
     const confetti = document.createElement("div");
     confetti.classList.add("confetti");
     document.body.appendChild(confetti);
-    
+
     const colors = ["red", "blue", "green", "yellow", "purple", "orange"];
     confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-    
+
     confetti.style.left = Math.random() * window.innerWidth + "px";
     confetti.style.top = "-10px";
-    
+
     const animationDuration = Math.random() * 3 + 2;
     confetti.style.transition = `transform ${animationDuration}s linear, opacity ${animationDuration}s ease-in`;
-    
+
     requestAnimationFrame(() => {
         confetti.style.transform = `translateY(${window.innerHeight + 10}px) rotate(${Math.random() * 360}deg)`;
         confetti.style.opacity = 0;
     });
-    
+
     setTimeout(() => {
         confetti.remove();
     }, animationDuration * 1000);
-    
+
 }
 
 function stopConfetti() {
